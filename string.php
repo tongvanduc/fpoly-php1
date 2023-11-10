d82f53673e1b5e6045927e0dbfa049e60165fad9
Tống Văn Đức <tongduc315@gmail.com>
2023-11-10 14:21:51 +0700

    trim

---
+++ string.php
@@ -0,0 +1,25 @@
+<?php
+
+// trim — Strip whitespace (or other characters) from the beginning and end of a string
+
+// html_entity_decode — Convert HTML entities to their corresponding characters
+
+// htmlentities — Convert all applicable characters to HTML entities
+
+// htmlspecialchars_decode — Convert special HTML entities back to characters
+
+// htmlspecialchars — Convert special characters to HTML entities
+
+// str_contains — Determine if a string contains a given substring
+
+// str_pad — Pad a string to a certain length with another string
+
+// str_replace — Replace all occurrences of the search string with the replacement string
+
+// strcmp — Binary safe string comparison
+
+// strip_tags — Strip HTML and PHP tags from a string
+
+// strtolower — Make a string lowercase
+
+// strtoupper — Make a string uppercase
