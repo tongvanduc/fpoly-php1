fb8bc6e776d45563e65bb8dcc8ed032830377c8e
Tống Văn Đức <tongduc315@gmail.com>
2024-01-05 10:56:27 +0700

    Update basic.php

--- basic/basic.php
+++ basic/basic.php
@@ -1,72 +1,21 @@
-<?php
-
-// Đây là biến
-$variable1;
-$variable2 = "Ahihi";
-$variable3 = 1111;
-$variable4 = true;
-$variable5 = null;
-$variable6 = 10.25;
-$variable7 = [1, 3, 5];
-$variable8 = array(1, 3, 5);
-
-/* 
-    Thao tác cơ bản với biến
-    Toán tử phổ biến: Cộng, trừ, nhân, chia, lấy dư
-*/
-$a = 1;
-$b = 2;
-$c1 = $a + $b;
-$c2 = $a - $b;
-$c3 = $a * $b;
-$c4 = $a / $b;
-$c5 = $a % $b;
-
-echo '$a  = ' . $a . '<br>';
-echo '$b  = ' . $b . '<br>';
-echo '$c1 = ' . $c1 . '<br>';
-echo '$c2 = ' . $c2 . '<br>';
-echo '$c3 = ' . $c3 . '<br>';
-echo '$c4 = ' . $c4 . '<br>';
-echo '$c5 = ' . $c5 . '<br>';
-
-$array1 = [1, 2, 3, 4, 5, 6];
-$array2 = [
-    'ahihi' => [
-        "key1" => "Value1",
-        "key2" => "Value2",
-        "key3" => "Value3",
-    ],
-    [
-        "key1" => "Value1",
-        "key2" => "Value2",
-        "key3" => "Value3",
-    ]
-];
+<?php 
 
 echo "<pre>";
-var_dump($array1);
-echo '<br>';
-print_r($array2);
 
-// Biểu thức điều kiện + foreach
-/*
-    Hiển thị danh sách số chia hết cho 2
-*/
-$array3 = [1, 2, 3, 4, 5, 6, 7, 8];
-$array4 = []; // Mảng chứa Số chia hết cho 2
-$array5 = []; // Mảng chứa Số KHÔNG chia hết cho 2
+// Biến
+$bien1 = 'Ahihi';
+$bien2 = 2;
+$bien3 = [3, 3, 4];
+$bien4 = null;
+$bien5 = true;
 
-foreach ($array3 as $key => $value) {
-    if ($value % 2 == 0) {
-        $array4[] = $value;
-    } else {
-        array_push($array5, $value);
-    }
-}
+var_dump($bien1);
+var_dump($bien2);
+var_dump($bien3);
+var_dump($bien4);
+var_dump($bien5);
 
-echo 'Mảng chứa Số chia hết cho 2' . '<br>';
-print_r($array4);
+// Hằng số
+const CONST_STATUS = 1;
 
-echo 'Mảng chứa Số KHÔNG chia hết cho 2' . '<br>';
-print_r($array5);
+define('CONST_STATUS_2', 2);
